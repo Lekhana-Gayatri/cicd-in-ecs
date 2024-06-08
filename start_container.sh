f355bd9056fec0872e006e1da4e64fcdb1e0fe85
Lekhana Gayatri <lekhanagayatri@gmail.com>
2024-06-08 18:19:17 +0530

    adding the files

---
+++ start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+docker pull lekhana2004/django-code-build:v1
+echo
+
+CONTAINERID=$(docker run -d -p 8000:8000 --label demoBuildContainer lekhana2004/django-code-build:v1)
+echo $CONTAINERID
